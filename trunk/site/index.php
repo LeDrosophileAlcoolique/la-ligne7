198d4a134befd5df20983342101cbc10f5ab5c1b
remi.weng@epita.fr <remi.weng@epita.fr@c54e8cd0-d000-7c51-e7b1-37f6b477e047>
2011-02-21 16:27:59 +0000

    Site version 2 pour la soutenance 2

--- trunk/site/index.php
+++ trunk/site/index.php
@@ -1,6 +1,13 @@
 <?php
 require("include/include.php");
 
+// Pagination
+$nbrMessageParPage = 3;
+$nbrPage = nbrPage("news", $nbrMessageParPage);
+                         
+// $_GET pagination
+$page = page($nbrPage, "FIRST");
+
 echo $miseHTML['header'];
 ?>
 <h1>Présentation de jeu</h1>
@@ -11,6 +18,21 @@ d’ouvrir la grille qui ferme la salle dans laquelle vous êtes, vous avez just
 temps de le voir qu’il disparait aussitôt en poussant de grands hurlements.</p>
 <p>Vous êtes donc seul sans aide contre des ennemis que vous ne connaissez pas
 encore (vous apprendrez à les connaitre très très vite. . .)</p>
+
 <?php
+// Requête Sql
+$sql = 'SELECT news, UNIX_TIMESTAMP(date) FROM news ORDER BY date DESC LIMIT '.($page-1)*$nbrMessageParPage.','.$nbrMessageParPage;
+$req = mysql_query($sql);
+
+while ($row = mysql_fetch_array($req)) {
+    echo '<p class="date">'.getDateFr("l j F Y à G:i", intval($row['UNIX_TIMESTAMP(date)'])).',</p> ';
+    echo '<p class="news">'.utf8_encode(nl2br($row['news'])).'</p>';
+}
+                
+// Pagination
+echo '<p>Page : ';
+echo echoPage($page, $nbrPage, "index", "", "FIRST");
+echo '</p>';
+  
 echo $miseHTML['footer'];
 ?>
