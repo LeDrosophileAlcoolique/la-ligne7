825f488593a11943d592860dd2e9cdc7fd44c4d3
remi.weng@epita.fr <remi.weng@epita.fr@c54e8cd0-d000-7c51-e7b1-37f6b477e047>
2011-02-22 18:12:57 +0000

    Mise à jour de la page téléchargement

--- site/telechargement.php
+++ site/telechargement.php
@@ -5,6 +5,10 @@ echo $miseHTML['header'];
 ?>
 <h1>Cahier des charges</h1>
 <p>Le cachier des charges est disponble en <a href="telechargement/cahier_des_charges.pdf">cliquant ici</a>.</p>
+<h1>Le rapport de la soutenance 2</h1>
+<p>La rapport est disponible en <a href="telechargement/rapport.pdf">cliquant ici</a>.</p>
+<h1>Le jeu</h1>
+<p>Et le plus important, le jeu est disponible gratuitement en téléchargement en <a href="telechargement/ligne7.zip">cliquant ici</a>.</p>
 <?php
 echo $miseHTML['footer'];
 ?>
