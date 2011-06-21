de6c9d22108bb3443a1822611711427d25cccedb
remi.weng@epita.fr <remi.weng@epita.fr@c54e8cd0-d000-7c51-e7b1-37f6b477e047>
2011-06-21 15:52:08 +0000

    Site mise à jour

--- trunk/site/team.php
+++ trunk/site/team.php
@@ -38,6 +38,23 @@ Visual Basic.</p>
 <p>Ce sont les jeux vidéos qui ont poussé ma curiosité vers l’informatique.
 Ce projet me permet donc d’assouvir ma curiosité en créant moi même ce
 qui m’a entraîné à aller vers l’informatique.</p>
+
+<h1>Nos ressentis</h1>
+
+<h2>Arnaud Gagne</h2>
+<p>Tout d'abord, je dois dire que c'était une très bonne expérience, que je ne regrette absolument pas !</p>
+<p>Chercher des solutions à des problèmes qui paraissent être impossibles à chaque apport nécessaire au jeu (la collision, la collision dans les coins, le saut du joueur) était très dificile, tant ces problèmes étaient frustrants, mais à chaque fois que je trouvais la solution c'était une grande joie qui surpassait la frustration.</p>
+<p>Malgré divers difficultés au sein du groupe, nous avons réussi a finir un jeu qui paraissait de plus en plus impossible à fournir au fur et a mesure de son avancement, grâce a la ténacité des membres du groupe. De plus, chercher soi-même et découvrir le langage était vraiment très intéressant, à chaque soutenance notre jeu changeait de visage et ressemblait de plus en plus à un vrai jeu.</p>
+
+<h2>Rémi Weng</h2>
+<p>Ce projet a été moi pour très enrichissant puisque nous avons appris à mener un projet de A à Z. Nous avons aussi appris à nous débrouiller tout seul en faisant nous-mêmes des recherches. C'est seulement après une année de travail acharné sur ce projet que j'ai compris qu'un projet était aussi complexe (on ne peut donc pas improviser pour un projet, tout doit être prévu à l'avance). Même si cela a été dur, je suis très fier du résultat que nous avons accompli ensemble.</p>
+<p>L'expérience apportée par ce projet ne peut être appris à travers les cours. Ce projet est donc un très bon moyen de connaître nos capacités et nos limites.
+Je n'hésiterai donc à refaire ce genre de projet c'est à dire de refaire un jeu de manière personnelle puisque nous avons déjà de très bonne base après avoir passé une année complète dessus.</p>
+
+<h2>Jacques Mahoudeaux</h2>
+<p>Eh bien, s’il y a une chose que l'on peut dire, c'est que le développement de ce projet aura été rythmé. C'est la première fois que je travaille sur un projet aussi important, et je dois avouer que ce fut vraiment une excellente expérience et que j'ai appris un nombre incalculable de choses. De plus, le résultat final rend vraiment bien et pour un premier projet, je le trouve de qualité !</p>
+<p>C'est pourquoi je suis très heureux du résultat auquel notre groupe a pu arriver. Je suis également très heureux de pouvoir dire que j'ai participé à la création de ce projet, car il s'agit d'une première pour moi et maintenant j'ai la conviction que cette activité me plait.</p>
+<p>J'ai été ravi de travailler avec toute l'équipe RETP qui, il faut le dire, possède une ambiance très conviviale et j'ai eu ce que je voulais en travaillant sur se projet : passer de l'autre côté de l'écran. Ceci m'a permit de découvrir le métier que je souhaitais vraiment avoir plus tard, d'en connaitre les meilleurs mais aussi les pires moments.</p>
 <?php
 echo $miseHTML['footer'];
 ?>
--- trunk/site/telechargement.php
+++ trunk/site/telechargement.php
@@ -5,14 +5,24 @@ echo $miseHTML['header'];
 ?>
 <h1>Cahier des charges</h1>
 <p>Le cachier des charges est disponble en <a href="telechargement/cahier_des_charges.pdf">cliquant ici</a>.</p>
+
 <h1>Le rapport de la soutenance 1</h1>
 <p>La rapport est disponible en <a href="telechargement/rapport1.pdf">cliquant ici</a>.</p>
+
 <h1>Le rapport de la soutenance 2</h1>
 <p>La rapport est disponible en <a href="telechargement/rapport2.pdf">cliquant ici</a>.</p>
+
 <h1>Le rapport de la soutenance 3</h1>
 <p>La rapport est disponible en <a href="telechargement/rapport3.pdf">cliquant ici</a>.</p>
-<h1>Le jeu</h1>
-<p>Et le plus important, le jeu est disponible gratuitement en téléchargement en <a href="telechargement/ligne7.zip">cliquant ici</a>.</p>
+
+<h1>Le rapport de la soutenance 4</h1>
+<p>La rapport est disponible en <a href="telechargement/rapport4.pdf">cliquant ici</a>.</p>
+
+<h1>Le jeu (l'installeur)</h1>
+<p>Et le plus important, le jeu est disponible gratuitement en téléchargement en <a href="telechargement/La Ligne 7.exe">cliquant ici</a>.</p>
+
+<h1>Le manuel</h1>
+<p>La manuel est disponible en <a href="telechargement/manuel.pdf">cliquant ici</a>.</p>
 <?php
 echo $miseHTML['footer'];
 ?>
