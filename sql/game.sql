dd536ab70509b19401090c2f095559648e2164f5
remi.weng@epita.fr <remi.weng@epita.fr@c54e8cd0-d000-7c51-e7b1-37f6b477e047>
2011-06-21 16:14:35 +0000

    Sql

--- sql/game.sql
+++ sql/game.sql
@@ -1,14 +1,4 @@
-﻿-- phpMyAdmin SQL Dump
--- version 3.3.9
--- http://www.phpmyadmin.net
---
--- Serveur: localhost
--- Généré le : Mar 26 Avril 2011 à 14:18
--- Version du serveur: 5.5.8
--- Version de PHP: 5.3.5
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
+﻿
 --
 -- Base de données: `test`
 --
@@ -46,7 +36,7 @@ CREATE TABLE IF NOT EXISTS `news` (
   `news` mediumtext NOT NULL,
   `date` datetime NOT NULL,
   KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
 
 --
 -- Contenu de la table `news`
@@ -62,4 +52,7 @@ INSERT INTO `news` (`id`, `news`, `date`) VALUES
 (8, 'Les évitements d''obstacles sont désormais implémentés : les zombies ne deviendrons plus inerte dès qu''ils toucheront un pylône ou un mur. Des zombies un peu plus intelligent.', '2011-04-12 12:30:10'),
 (9, 'Notre équipe viens de perdre Thibault pour qui l''appel du Sud a été trop fort. Nous nous retrouvons donc à 3 pour finir le jeu. Nous venons de perdre un membre face aux zombies !', '2011-03-15 17:26:20'),
 (10, 'Une très grand avancée esthétique puisque nous avons intégré des textures aux modèles 3D. Un jeu radicalement plus beau !', '2011-04-22 22:25:18'),
-(11, 'Le jeu a été presque totalement recodé pour pouvoir chargé plus de zombie, tirer plus de fois sans aucun lag !', '2011-03-30 17:35:00');
+(11, 'Le jeu a été presque totalement recodé pour pouvoir chargé plus de zombie, tirer plus de fois sans aucun lag !', '2011-03-30 17:35:00'),
+(12, 'Aujourd''hui, nous avons commencé à implémenter les vidéos dans le code. C''est à ce moment là qu''on a remarqué que l''on avait commencé le jeu sous xna 3.0. Or, les vidéos ne sont importable que sur xna 3.1. On a alors dû upgrade le projet pour le passer en 3.1 et là, on a remarqué que plein de bug se sont apparu et on a dû revoir tout le code.\r\n\r\nPendant cette semaine de rush, nous avons passé pas mal de temps à coder ainsi qu''à créer les différentes cartes avec leurs modèles. C''était sympa de pouvoir modifier les décors avec des textures différentes.\r\n\r\nVoilà nous venons de passer nos partiels. Maintenant il faut qu''on rattrape le retard qu''on a eu lors des autres soutenances.', '2011-06-17 21:47:01'),
+(13, 'Nous attaquons aujourd''hui le réseau qui va permettre à plusieurs personnes de jouer ensemble sur deux ordinateurs différents. Nous avons le choix d''utiliser le réseau de la bibliothèque de xna ou de charp. Pour l''instant, nous optons pour la première solution car xna est avant tout un framework pour faire des jeux. Donc, des fonctions plus proches de ce que nous avons besoin pour notre jeu.\r\n\r\nNous allons donc passer beaucoup de temps à comprendre comment cela marche en faisant des recherches. Cependant, nous avons pas trouvé énormément de documents traitant du réseau.', '2011-05-27 09:31:37'),
+(14, 'Cela fait depuis deux jours que nous attaquons le menu, nous avons très bien avancé au niveau code. Mais reste encore la partie visuelle.\r\n\r\nNous avons un énorme problème puisque les deux menus (le nouveau et l''ancien) ne fonctionne pas du tout de la même manière. Nous avons dû recoder une partie.', '2011-06-13 12:24:33');
